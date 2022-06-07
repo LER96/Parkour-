8408dbdf9316637e7dd756c6d7158ffe4df62418
LER96 <93140643+LER96@users.noreply.github.com>
2022-06-07 14:21:39 +0300

    New scenes + slope movement

--- Assets/Scripts/PlayerFps.cs
+++ Assets/Scripts/PlayerFps.cs
@@ -8,16 +8,28 @@ public class PlayerFps : MonoBehaviour
     float horizontal;
     float vertical;
     Vector3 moveDir;
+    Vector3 slopeDir;
+
+    [Header("Movement")]
     public float speed=5;
-    public float drag = 5;
-    public float jump = 5;
+    public float moveMultiplier = 10;
+
+    [Header("Drag Control")]
+    public float groundDrag = 5;
+    public float airDrag = 1.5f;
+
+    [Header("Jump")]
+    public float jumpForce = 5;
+    public float airMultiplier = 0.4f;
     public Rigidbody rb;
 
+    [Header ("Ground Check")]
     public Transform groundCheck;
     public float groundDist = 0.3f;
     public LayerMask groundMask;
     public bool isGrounded;
 
+    RaycastHit slopeHit;
     private void Start()
     {
         rb.freezeRotation = true;
@@ -27,15 +39,43 @@ public class PlayerFps : MonoBehaviour
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(transform.up * jump, ForceMode.Impulse);
+            Jump();
         }
         MyInput();
         ControlDrag();
+
+        slopeDir = Vector3.ProjectOnPlane(moveDir, slopeHit.normal);
+    }
+
+    bool OnSlope()
+    {
+        if (Physics.Raycast(groundCheck.position, Vector3.down, out slopeHit, groundMask))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
     private void ControlDrag()
     {
-        rb.drag = drag;
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
 
     private void MyInput()
@@ -52,6 +92,18 @@ public class PlayerFps : MonoBehaviour
 
     private void MovePlayer()
     {
-        rb.AddForce(moveDir.normalized * speed, ForceMode.Acceleration);
+        if (isGrounded && ! OnSlope())
+        {
+            rb.AddForce(moveDir.normalized * speed * moveMultiplier, ForceMode.Acceleration);
+        }
+        else if(isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeDir.normalized * speed * moveMultiplier, ForceMode.Acceleration);
+        }
+        else if(!isGrounded)
+        {
+            rb.AddForce(moveDir.normalized * speed * moveMultiplier* airMultiplier, ForceMode.Acceleration);
+        }
+
     }
 }
