82c054348404822755ac48481bac6450c97043f0
layanmeta <layan.meta@gmail.com>
2022-07-03 16:09:40 +0300

    cutscene and press f to skip done and loading from main menu

--- Assets/Scripts/Layan's Scripts 1/GameManager.cs
+++ Assets/Scripts/Layan's Scripts 1/GameManager.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("GameObjects")]
     [SerializeField] GameObject quitGame;
     [SerializeField] GameObject startGame;
-
     [SerializeField] PlayableDirector cutscene;
-
+    [SerializeField] private double _skipIntro = 40f;
     [SerializeField] GameObject cutsceneCamera;
     [SerializeField] GameObject mainCamera;
-    [SerializeField]GameObject reference;
+    [SerializeField] GameObject playerReference;
+    [SerializeField] GameObject loadingUI;
+    [SerializeField] Slider loadingSlider;
+
+
+    [Header("References")]
     private PlayerMovement1 player;
     private PlayerCamMovement playerCam;
     private void Start()
     {
-        player = reference.GetComponent<PlayerMovement1>();
+        player = playerReference.GetComponent<PlayerMovement1>();
         playerCam = mainCamera.GetComponent<PlayerCamMovement>();
         player.enabled = false;
         player.animator.enabled = false;
         playerCam.enabled = false;
         cutscene = GetComponent<PlayableDirector>();
+        loadingUI.SetActive(false);
     }
 
     private void Update()
@@ -33,13 +40,18 @@ public class GameManager : MonoBehaviour
 
     private void CutsceneDone()
     {
-        if (cutscene.state != PlayState.Playing)
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log("done");
+            cutscene.time = _skipIntro;
+        }
+        else if (cutscene.state != PlayState.Playing)
+        {
+            
             player.enabled = true;
             player.animator.enabled = true;
             cutsceneCamera.SetActive(false);
             playerCam.enabled = true;
+            Debug.Log("done");
         }
     }
 
@@ -50,7 +62,22 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        //SceneManager.LoadScene(1);
+        StartCoroutine(LoadAsync(1));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingUI.SetActive(true);
+
+        while (operation.isDone == false)
+        {
+            float barProgress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingSlider.value = barProgress;
+            yield return null;
+        }
     }
 
     public void NextLvl()
