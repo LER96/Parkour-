ac51c0afe6dcb8a60c0e5ac6d8662855545aedc6
layanmeta <layan.meta@gmail.com>
2022-07-04 14:39:59 +0300

    fixed bugs

--- Assets/Scripts/Layan's Scripts 1/CutsceneHandler.cs
+++ Assets/Scripts/Layan's Scripts 1/CutsceneHandler.cs
@@ -1,27 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Playables;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class CutsceneHandler : MonoBehaviour
 {
     [Header("GameObjects")]
-    [SerializeField] GameObject quitGame;
-    [SerializeField] GameObject startGame;
+   
     [SerializeField] PlayableDirector cutscene;
     [SerializeField] private double _skipIntro = 40f;
     [SerializeField] GameObject cutsceneCamera;
     [SerializeField] GameObject mainCamera;
     [SerializeField] GameObject playerReference;
-    [SerializeField] GameObject loadingUI;
-    [SerializeField] Slider loadingSlider;
-
 
     [Header("References")]
     private PlayerMovement1 player;
     private PlayerCamMovement playerCam;
+
     private void Start()
     {
         player = playerReference.GetComponent<PlayerMovement1>();
@@ -30,7 +25,6 @@ public class GameManager : MonoBehaviour
         player.animator.enabled = false;
         playerCam.enabled = false;
         cutscene = GetComponent<PlayableDirector>();
-        loadingUI.SetActive(false);
     }
 
     private void Update()
@@ -46,42 +40,12 @@ public class GameManager : MonoBehaviour
         }
         else if (cutscene.state != PlayState.Playing)
         {
-            
+
             player.enabled = true;
             player.animator.enabled = true;
             cutsceneCamera.SetActive(false);
             playerCam.enabled = true;
             Debug.Log("done");
         }
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void StartGame()
-    {
-        //SceneManager.LoadScene(1);
-        StartCoroutine(LoadAsync(1));
-    }
-
-    IEnumerator LoadAsync(int sceneIndex)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-
-        loadingUI.SetActive(true);
-
-        while (operation.isDone == false)
-        {
-            float barProgress = Mathf.Clamp01(operation.progress / 0.9f);
-            loadingSlider.value = barProgress;
-            yield return null;
-        }
-    }
-
-    public void NextLvl()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Assets/Scripts/Layan's Scripts 1/UIHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    [Header("GameObjects")]
+    [SerializeField] GameObject quitGame;
+    [SerializeField] GameObject startGame;
+    [SerializeField] GameObject loadingUI;
+    [SerializeField] Image loadingImage;
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        //SceneManager.LoadScene(1);
+        StartCoroutine(LoadAsync(1));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingUI.SetActive(true);
+
+        while (operation.isDone == false)
+        {
+            float barProgress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingImage.fillAmount = barProgress;
+            yield return null;
+        }
+    }
+
+    public void NextLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
